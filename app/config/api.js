86df6372f87a959584e48cb49bc35c638f81a19c
durai87787 <durairaj.unipro@gmail.com>
2026-03-24 15:41:50 +0530

    server.js modified

--- app/config/api.js
+++ app/config/api.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = "http://192.168.1.5:3000";
    // export const BASE_URL = "http://localhost:3000";
    export const BASE_URL = "https://smart-bi-production.up.railway.app";
-
+                                   
    
 
   //  import { Platform } from "react-native";
