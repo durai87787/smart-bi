b55af0310a8e65faf398535bd1ee444770294a32
durai87787 <durairaj.unipro@gmail.com>
2026-04-10 17:51:06 +0530

    modifyapi

--- app/config/api.js
+++ app/config/api.js
@@ -1,12 +1,12 @@
 // export const BASE_URL = "http://192.168.1.5:3000";
-  export const BASE_URL = "http://localhost:3000";
-  //
-  
-  // export const BASE_URL = "https://smart-bi-production.up.railway.app";
-                                   
-   
+// export const BASE_URL = "http://localhost:3000";
+//
 
-  //  import { Platform } from "react-native";
+export const BASE_URL = "https://smart-bi-production.up.railway.app";
+
+
+
+//  import { Platform } from "react-native";
 
 // const LOCAL_IP = "26.14.83.241"; // your PC IP
 // const LOCAL_IP = "10.74.243.179"; // your PC IP
